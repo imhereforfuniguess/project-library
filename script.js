57e291aad55fbfdf15818bb11a431025905b27bd
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-12-12 19:41:40 +0200

    Add 3 functions
    Just trying committing through VSC

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 let myLibrary = []
 
 let book = function(title, author, pages, read){
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read = read
+	this.title = title,
+	this.author = author,
+	this.pages = pages,
+	this.read = read
 }
-
-book.prototype.info = function (){
-  return [this.title, this.pages]
-}
-
 
 function addBookToLibrary(addedBook) {
-  myLibrary.push(addedBook)
-  return
+	myLibrary.push(addedBook)
+	return
 }
+
+function displayBooks() {
+	for (let i = 0; i < myLibrary.length; i++){
+	}
+}
+
+function createBookList() {
+	const bookList = document.createElement('ul')
+}
+
+function createBookElements(bookStats) {
+	const bookElement = document.createElement('li')
+	bookElement.textContent = bookStats
+}
+
 
 // console.table(theHobbit.info())
 const theHobbit = new book("The Hobbit","J.R.R Tolkien", 250, 0)
 const harryPotter = new book("Harry Potter","J. K. Rowling", 678, 1)
+const dune = new book("Dune","F. Herbert", 412, 1)
 
 addBookToLibrary(theHobbit)
 addBookToLibrary(harryPotter)
