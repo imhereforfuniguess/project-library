7b669daf1241a1a2ebd8a6e541154ec5fd36acdb
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-12-12 20:08:04 +0200

    Add functions for adding Books to the main div

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ let book = function(title, author, pages, read){
 
 function addBookToLibrary(addedBook) {
 	myLibrary.push(addedBook)
-	return
 }
 
 function displayBooks() {
@@ -19,12 +18,20 @@ function displayBooks() {
 
 function createBookList() {
 	const bookList = document.createElement('ul')
+	bookList.textContent = "BOOK LIST TEST"
+	libraryPanel.appendChild(bookList)
 }
 
-function createBookElements(bookStats) {
-	const bookElement = document.createElement('li')
-	bookElement.textContent = bookStats
+function createBookElements(book) {
+	for (let i = 0; i < Object.keys(book).length; i++){
+		const bookElement = document.createElement('li')
+		bookElement.textContent = Object.values(book)[i]
+		libraryPanel.appendChild(bookElement)
+	}
 }
+
+
+const libraryPanel = document.querySelector('.container')
 
 
 // console.table(theHobbit.info())
@@ -32,6 +39,14 @@ const theHobbit = new book("The Hobbit","J.R.R Tolkien", 250, 0)
 const harryPotter = new book("Harry Potter","J. K. Rowling", 678, 1)
 const dune = new book("Dune","F. Herbert", 412, 1)
 
+
+console.log()
+
 addBookToLibrary(theHobbit)
 addBookToLibrary(harryPotter)
 console.table(myLibrary)
+
+createBookList()
+createBookElements(harryPotter)
+createBookList()
+createBookElements(theHobbit)
